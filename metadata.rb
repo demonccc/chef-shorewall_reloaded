519cfdd706597d8626c6548401f0b8e8958e0c5b
Claudio Cesar Sanchez Tejeda <claudio@bstriker.com>
2015-02-10 18:21:38 -0300

    Modified some files in order to fix some Foodcritic alerts

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email	'demonccc@gmail.com'
 license			'Apache 2.0'
 description		'Configures and install Shorewall'
 long_description	IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version			'0.3.1'
+version			'0.3.2'
 
 recipe                  'shorewall_reloaded', 'Configures and activates Shorewall firewall'
 
